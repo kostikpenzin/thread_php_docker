75c3eb2d3b6c2327217284593190588bc402801e
Kostik <penzin85@gmail.com>
2021-02-12 19:57:14 +0400

    init

--- Dockerfile
+++ Dockerfile
@@ -6,8 +6,8 @@ RUN git clone https://github.com/krakjoe/pthreads -b master /tmp/pthreads
 RUN docker-php-ext-configure /tmp/pthreads --enable-pthreads
 RUN docker-php-ext-install /tmp/pthreads
 
-RUN git clone https://github.com/kostikpenzin/thread_php_docker.git
-RUN cd thread_php_docker
+RUN git clone https://github.com/kostikpenzin/thread_php_docker.git -b master /var/www/thread_php_docker
+RUN cd /var/www/thread_php_docker
 
 WORKDIR /var/www/thread_php_docker
 RUN echo | php index.php
--- index.php
+++ index.php
@@ -1,64 +1,59 @@
 <?php
-class TestWork extends Threaded {
-    
-    protected $complete;
-    public function __construct($pData) {
-        
-        $this->complete = false;
-        $this->testData = $pData;
-    }
-    
-    public function run() {
-        usleep(2000000); //sleep 2 seconds to simulate a large job
-        $this->complete = true;
-    }
-    public function isDone() {
-        return $this->complete;
-    }
+
+class MultiThreadExample extends Thread {
+
+  private $threadId;
+  public $data;
+
+  public function __construct(int $id) {
+
+    $this->threadId = $id;
+    $this->data = $id . ":" . date('H:i:s');
+  }
+
+  public function run() {
+
+    //thread started
+    echo 'thread ' . $this->threadId . "  started.\n";
+
+    //sleep for <threadId> seconds
+    sleep($this->threadId);
+
+    //thread ended with timestamp
+    echo 'thread ' . $this->threadId . " ended at " . date('H:i:s') . "\n";
+  }
 }
-class RunPool extends Pool {
-    public $data = array(); 
-    private $numTasks = 0; 
-    /**
-     * override the submit function from the parent
-     * to keep track of our jobs
-     */
-    public function submit(Threaded $task) {
-        $this->numTasks++;
-        parent::submit($task);
-    }
-    /**
-     * used to wait until all workers are done
-     */
-    public function process() {
-        // Run this loop as long as we have
-        // jobs in the pool
-        while (count($this->data) < $this->numTasks) {
-            $this->collect(function (TestWork $task) {
-                // If a task was marked as done
-                // collect its results
-                if ($task->isDone()) {
-                    $tmpObj = new stdclass();
-                    $tmpObj->complete = $task->complete;
-                    //this is how you get your completed data back out [accessed by $pool->process()]
-                    $this->data[] = $tmpObj;
-                }
-                return $task->isDone();
-            });
-        }
-        // All jobs are done
-        // we can shutdown the pool
-        $this->shutdown();
-        return $this->data;
-    }
-}
-$pool = new RunPool(100);
-$testData = 'asdf';
-for($i=0;$i<5;$i++) {
-    $pool->submit(new TestWork($testData));
-}
-$retArr = $pool->process(); //get all of the results
-echo '<pre>';
-print_r($retArr); //return the array of results (and maybe errors)
-echo '</pre>';
-?>
+ 
+$threads = [];
+$i = 0;
+do {
+
+  $i++;
+
+  $threads[$i] = new MultiThreadExample($i);
+
+  //start thread job
+  $threads[$i]->start();
+
+  //echo thread data
+  echo $threads[$i]->data . "\n";
+
+} while($i < 5);
+
+echo "sleeping for 5 seconds... \n";
+
+sleep(5);
+
+echo "now attempting to join...\n";
+
+$i = 0;
+do {
+
+  $i++;
+
+  //join thread
+  $threads[$i]->join();
+
+} while($i < 50);
+
+echo "threads already finished executing. Nothing to join now.\n";
